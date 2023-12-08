716358d2e416d0dfb9b6a4c0bd27678cfcc77ece
Micah <139593153+G303K@users.noreply.github.com>
2023-12-07 19:52:47 -0500

    Created models for Thought, User, & added index.js

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: { type: Types.ObjectId, default: () => new Types.ObjectId() },
+    reactionBody: { type: String, required: true, min: 1, maxlength: 280 },
+    username: { type: String, required: true },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  { toJSON: { virtuals: true, getters: true }, id: false }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: { type: String, required: true },
+    reactions: [reactionSchema],
+  },
+  { toJSON: { virtuals: true, getters: true }, id: false }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
