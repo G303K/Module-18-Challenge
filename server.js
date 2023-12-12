a4253e97bfd16ee79c4ae51982deca0d9ff34eb4
Micah <139593153+G303K@users.noreply.github.com>
2023-12-12 16:22:59 -0500

    npm install and fixed server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json(), express.urlencoded({ extended: true }), express.static('public'));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/social-network-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  debug: true,
 });
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
