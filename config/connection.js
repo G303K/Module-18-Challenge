1566984879d27a4acb7033c8927560214a03982f
Micah <139593153+G303K@users.noreply.github.com>
2023-12-14 18:04:12 -0500

    Fixed connect.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/Module-18-Challenge",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Module-18-Challenge",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
