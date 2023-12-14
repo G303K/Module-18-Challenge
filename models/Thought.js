7ed19ffa01e3a1745f86a8c3175e5810cd96db02
Micah <139593153+G303K@users.noreply.github.com>
2023-12-14 12:08:47 -0500

    Shortened models

--- models/Thought.js
+++ models/Thought.js
@@ -1,30 +1,22 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const reactionSchema = new Schema(
+const ReactionSchema = new Schema(
   {
-    reactionId: { type: Types.ObjectId, default: () => new Types.ObjectId() },
-    reactionBody: { type: String, required: true, min: 1, maxlength: 280 },
+    reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
+    reactionBody: { type: String, required: true, maxlength: 280 },
     username: { type: String, required: true },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
+    createdAt: { type: Date, default: Date.now, get: (timestamp) => dateFormat(timestamp) },
   },
-  { toJSON: { virtuals: true, getters: true }, id: false }
+  { toJSON: { getters: true }, id: false }
 );
 
 const ThoughtSchema = new Schema(
   {
-    thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
+    thoughtText: { type: String, required: "Thought is Required", minlength: 1, maxlength: 280 },
+    createdAt: { type: Date, default: Date.now, get: (timestamp) => dateFormat(timestamp) },
     username: { type: String, required: true },
-    reactions: [reactionSchema],
+    reactions: [ReactionSchema],
   },
   { toJSON: { virtuals: true, getters: true }, id: false }
 );
@@ -36,3 +28,4 @@ ThoughtSchema.virtual("reactionCount").get(function () {
 const Thought = model("Thought", ThoughtSchema);
 
 module.exports = Thought;
+
--- models/User.js
+++ models/User.js
@@ -2,8 +2,18 @@ const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema(
   {
-    username: { type: String, unique: true, required: true, trim: true },
-    email: { type: String, required: true, unique: true, match: [/.+@.+\..+/] },
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: "Username is Required",
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: "Username is Required",
+      match: [/.+@.+\..+/],
+    },
     thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
     friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
